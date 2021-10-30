b3f03ad8576caca3bc70cbef4eacd55595684404
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-30 11:36:55 +0600

    feat: tailwindcss installed, eslintrc rules updated and tailwindcss className used

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function App() {
     return (
-        <>
-            <h1>Hello World!</h1>
-            <h6>Give a star if this helps you!</h6>
-        </>
+        <div className="flex justify-center items-center min-h-screen font-mono bg-gray-200 text-center">
+            <div>
+                <h1 className="text-4xl font-bold">Hello World!</h1>
+                <h6>If you liked the repo then kindly support it by giving it a star!</h6>
+            </div>
+        </div>
     );
 }
 
